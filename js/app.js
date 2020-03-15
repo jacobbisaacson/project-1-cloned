890d50722484eac067c2276598ac606f8a6ee644
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-03-15 13:59:59 -0500

    making sure dir is correct

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 console.log("project 1");
+
+const holes = document.querySelectorAll('.hole')
+const scoreBoard = document.querySelectorAll('.scoreBoard')
+const moles = document.querySelectorAll('mole')
+
+
+
+
+
+
+
+
+
+
+
+
+
